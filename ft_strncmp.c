22733b2b6770e8b6b088ee3eead1d70b6562a7ab
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2021-11-26 19:14:34 +0100

    blblbl

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,16 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned long	i;
 
 	i = 0;
-	while (i < n && s1[i] != '\0' && s2[i] != '\0')
+	if (n == 0)
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else
-		{
-			i++;
-		}
+		return (0);
 	}
-	return (s1[i] - s2[i]);
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
