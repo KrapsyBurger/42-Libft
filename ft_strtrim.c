9e37ddf5b972a4eed4d15fff600ce833a92971dd
MacBook Pro <macbookpro@macbooks-mbp.home>
2021-11-30 12:15:39 +0100

    blblbl

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -41,13 +41,20 @@ unsigned long	ft_strcomp2(const char *s1, const char *s2)
 unsigned long	ft_firstcheck(const char *s1, const char *s2)
 {
 	unsigned long	i;
+	unsigned long	j;
 
 	i = 0;
-	while (s1[i] == s2[i])
+	j = 0;
+	while (s2[j] != '\0')
 	{
-		i++;
+		while (s1[i] == s2[j])
+		{
+			i++;
+			j = 0;
+		}
+		j++;
 	}
-	if (s1 + i == NULL)
+	if (s1[i] == '\0')
 	{
 		return (1);
 	}
@@ -62,12 +69,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 	unsigned long	m;
 
 	j = 0;
-	i = ft_strcomp(s1, set);
-	m = ft_strcomp2(s1, set);
 	if (ft_firstcheck(s1, set) == 1)
 	{
-		return(result = ft_strdup(""));
+		return(ft_strdup(""));
 	}
+	i = ft_strcomp(s1, set);
+	m = ft_strcomp2(s1, set);
 	if (s1 == NULL)
 		return (NULL);
 	result = ft_substr(s1, i, m - i + 1);
